6b62b9203bc0ce0677e72d34981a3a3b5114ce60
SantiagoMartinez <Martinezsantiago253@gmail.com>
2020-09-18 03:31:45 +0000

    My favorite language

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("My name is Santiago Martinez");
+		System.out.println("My favorite programming language is C++");
 	}
 }
 
